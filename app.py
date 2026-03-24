4d821ca279794ac4bb1216ba3ac1a489cc242c21
mjosehs <130803441+mjosehs@users.noreply.github.com>
2026-03-24 19:58:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
+import os
 
-app = Flask(__name__)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 def get_db():
     return sqlite3.connect("database.db")
