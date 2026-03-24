ab011057d5aa0bec0bf1a80c15f50e227a7195e5
mjosehs <130803441+mjosehs@users.noreply.github.com>
2026-03-24 20:02:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 import sqlite3
 import os
 
+app = Flask(__name__)
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
